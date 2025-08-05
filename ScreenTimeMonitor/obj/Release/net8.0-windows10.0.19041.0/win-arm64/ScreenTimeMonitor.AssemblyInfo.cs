358f8dd01ff767e961c18be028c1a81539303de3
Ugan Sivagnanenthirarajah <ugan.sivagnanenthirarajah@gmail.com>
2025-08-05 14:23:52 -0700

    fixes

--- ScreenTimeMonitor/obj/Release/net8.0-windows10.0.19041.0/win-arm64/ScreenTimeMonitor.AssemblyInfo.cs
+++ ScreenTimeMonitor/obj/Release/net8.0-windows10.0.19041.0/win-arm64/ScreenTimeMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenTimeMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92822b0c74e2edd4710ffb887475d63031f92933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8b4d11023d2862fe88430a6a33c01d5046cb168")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenTimeMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenTimeMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
