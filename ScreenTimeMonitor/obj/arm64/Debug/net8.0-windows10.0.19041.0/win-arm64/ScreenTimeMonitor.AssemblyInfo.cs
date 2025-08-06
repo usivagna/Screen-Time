31b74066d5d7ec836300947a8ce4beb6a9ffec1a
Ugan Sivagnanenthirarajah <ugan.sivagnanenthirarajah@gmail.com>
2025-08-05 17:01:38 -0700

    Update ScreenTimeMonitor project files and binaries
    - Updated sourcelink.json to point to the latest commit in the repository.
    - Modified XamlSaveStateFile.xml with updated assembly paths and timestamps.
    - Rebuilt apphost.exe and ScreenTimeMonitor.dll binaries, reflecting changes in the project.
    - Updated output.json with new generated files and XAML compilation logs.
    - Rebuilt reference and reference intermediate DLLs to ensure consistency with the latest changes.

--- ScreenTimeMonitor/obj/arm64/Debug/net8.0-windows10.0.19041.0/win-arm64/ScreenTimeMonitor.AssemblyInfo.cs
+++ ScreenTimeMonitor/obj/arm64/Debug/net8.0-windows10.0.19041.0/win-arm64/ScreenTimeMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenTimeMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+157c88a61798e80cc0f969a02c3759296a31406a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30792bda0ba5756ef39c7e66e0c33350d12ec3ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenTimeMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenTimeMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
